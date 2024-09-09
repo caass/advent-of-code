e4ab91443e333cb2625f81db8cd2817759aa8316
Cass Fridkin <cass@swag.lgbt>
2024-09-08 22:14:30 -0600

    Own up to the fact that I'm solving this suboptimally.

--- src/2015/1.rs
+++ src/2015/1.rs
@@ -56,46 +56,45 @@ fn part_1(input: &str) -> isize {
     //
     // Finally, we can move even faster by using SIMD instructions to process 16 characters at a time.
     // That solution is what we use here.
+    #[inline(always)]
+    fn sum_chunk(chunk: &[u8]) -> isize {
+        if chunk.len() == u8x16::LANES as usize {
+            let simd_chunk = u8x16::from(chunk);
+
+            // This is a little confusing, but the idea is to use SIMD to process 16 bytes at a time.
+            // The first thing to do is to compare the values in the SIMD register with `b'('`.
+            // Values that are equal to `b'('` will be set to `u8::MAX`, others to `0u8`.
+            let max_or_zero = simd_chunk.cmp_eq(u8x16::splat(DOWN));
+
+            // The next step is to `saturating_add` one, so the values will either be `1` or `255`.
+            let max_or_one = max_or_zero.saturating_add(u8x16::ONE);
+
+            // finally, we can extract the values, which are all either `1` or `255`.
+            let arr = max_or_one.to_array();
+
+            // This is the cool bit;
+            // - `255u8` has the same bitwise representation as `-1i8`
+            // - `1u8` has the same bitwise representation as `1i8`
+            // so we can just reinterpret the bytes!
+            // this could be faster if `wide` had a `reduce_add`
+            let negative_one_or_one = unsafe { std::mem::transmute::<[u8; 16], [i8; 16]>(arr) };
+
+            let inner_sum = negative_one_or_one.into_iter().sum::<i8>();
+            inner_sum.into()
+        } else {
+            chunk
+                .iter()
+                .copied()
+                .map(|byte| if byte == UP { 1 } else { -1 })
+                .sum()
+        }
+    }
 
     input
         .as_bytes()
         .par_chunks(u8x16::LANES.into())
         .map(sum_chunk)
         .sum()
-}
-
-#[inline(always)]
-fn sum_chunk(chunk: &[u8]) -> isize {
-    if chunk.len() == u8x16::LANES as usize {
-        let simd_chunk = u8x16::from(chunk);
-
-        // This is a little confusing, but the idea is to use SIMD to process 16 bytes at a time.
-        // The first thing to do is to compare the values in the SIMD register with `b'('`.
-        // Values that are equal to `b'('` will be set to `u8::MAX`, others to `0u8`.
-        let max_or_zero = simd_chunk.cmp_eq(u8x16::splat(DOWN));
-
-        // The next step is to `saturating_add` one, so the values will either be `1` or `255`.
-        let max_or_one = max_or_zero.saturating_add(u8x16::ONE);
-
-        // finally, we can extract the values, which are all either `1` or `255`.
-        let arr = max_or_one.to_array();
-
-        // This is the cool bit;
-        // - `255u8` has the same bitwise representation as `-1i8`
-        // - `1u8` has the same bitwise representation as `1i8`
-        // so we can just reinterpret the bytes!
-        // this could be faster if `wide` had a `reduce_add`
-        let negative_one_or_one = unsafe { std::mem::transmute::<[u8; 16], [i8; 16]>(arr) };
-
-        let inner_sum = negative_one_or_one.into_iter().sum::<i8>();
-        inner_sum.into()
-    } else {
-        chunk
-            .iter()
-            .copied()
-            .map(|byte| if byte == UP { 1 } else { -1 })
-            .sum()
-    }
 }
 
 /// Now, given the same instructions, find the position of the first character that causes him to enter the basement
@@ -109,9 +108,11 @@ fn sum_chunk(chunk: &[u8]) -> isize {
 /// What is the position of the character that causes Santa to first enter the basement?
 fn part_2(input: &str) -> usize {
     // Similarly, we could easily just iterate over the characters and find the index of the one
-    // that leads Santa to the basement. But we should be able to parallelize the process.
+    // that leads Santa to the basement. But we should be able to parallelize the process. In theory...
+    // TODO: Parallelize.
 
     let mut floor = 0;
+
     input
         .as_bytes()
         .iter()
