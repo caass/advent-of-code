5c9ee3c8cbf7402dc0d70203999766befca3b996
Cass Fridkin <cass@swag.lgbt>
2024-09-09 13:55:32 -0600

    Start problem 5

---
+++ src/2015/5.rs
@@ -0,0 +1,6 @@
+use crate::types::Problem;
+
+pub const PROBABLY_A_FIRE_HAZARD: Problem = Problem {
+    part_1: None,
+    part_2: None,
+};
--- src/2015/mod.rs
+++ src/2015/mod.rs
@@ -1,14 +1,14 @@
 use crate::types::ProblemSet;
 use crate::util::mod_days;
 
-mod_days!(1, 2, 3, 4);
+mod_days!(1, 2, 3, 4, 5);
 
 pub const PROBLEMS: ProblemSet = ProblemSet([
     Some(NOT_QUITE_LISP),
     Some(I_WAS_TOLD_THERE_WOULD_BE_NO_MATH),
     Some(PERFECTLY_SPHERICAL_HOUSES_IN_A_VACUUM),
     Some(THE_IDEAL_STOCKING_STUFFER),
-    None,
+    Some(PROBABLY_A_FIRE_HAZARD),
     None,
     None,
     None,
