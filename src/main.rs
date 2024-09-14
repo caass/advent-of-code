7b7433e69cddc959d778dc16adb5b542fa840923
Cass Fridkin <cass@swag.lgbt>
2024-09-13 22:30:26 -0600

    Give up and use clap for argument parsing

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,28 @@
 use std::io::{stdin, IsTerminal, Read};
 
-use eyre::{OptionExt, Report};
-use thiserror::Error;
+use clap::Parser;
+use eyre::{bail, OptionExt, Result};
 
 use advent_of_code::types::{Day, Part, Year};
 use advent_of_code::AOC;
 
-#[derive(Debug, Error)]
-#[error("usage: cat <FILE> | aoc <YEAR> <DAY> <PART>")]
-pub struct ParseError;
-fn main() -> eyre::Result<()> {
-    let mut args = std::env::args();
+#[derive(Debug, Parser)]
+struct AocArgs {
+    year: Year,
+    day: Day,
+    part: Part,
+}
 
-    let _exe_name = args.next().ok_or(ParseError)?;
-    let year: Year = args.next().ok_or(ParseError)?.parse()?;
-    let day: Day = args.next().ok_or(ParseError)?.parse()?;
-    let part: Part = args.next().ok_or(ParseError)?.parse()?;
+fn main() -> Result<()> {
+    let AocArgs { year, day, part } = AocArgs::parse();
 
-    let input = args.next().map(Ok::<_, Report>).unwrap_or_else(|| {
-        if stdin().is_terminal() {
-            Err(ParseError.into())
-        } else {
-            let mut buf = String::new();
-            stdin().read_to_string(&mut buf)?;
-            Ok(buf)
-        }
-    })?;
+    let input = if stdin().is_terminal() {
+        bail!("Expected input on stdin, try `cargo run --release -- {year} {day} {part} < path/to/input");
+    } else {
+        let mut buf = String::new();
+        stdin().read_to_string(&mut buf)?;
+        buf
+    };
 
     let f = AOC
         .get(year, day, part)
