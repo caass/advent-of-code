1d87db40667660d7502ac52acff6b3ec964b6fdb
Cass Fridkin <cass@swag.lgbt>
2024-09-16 09:45:11 -0600

    Implement `Letter`

--- src/2015/11.rs
+++ src/2015/11.rs
@@ -1,274 +1,18 @@
 use std::{
     fmt::{self, Display, Formatter, Write},
-    ops::{Index, IndexMut},
+    ops::{Add, AddAssign, Sub, SubAssign},
     str::FromStr,
 };
 
-use eyre::{bail, Report, Result};
-use rayon::{
-    iter::plumbing::{self, bridge, Producer, Reducer},
-    prelude::*,
-};
-use winnow::Parser;
+use eyre::{bail, eyre, Report, Result};
 
 use crate::types::{problem, Problem};
 
 pub const CORPORATE_POLICY: Problem = problem!(part1);
+const ASCII_LETTER_OFFSET: u8 = b'a';
 
-fn part1(input: &str) -> Result<Password> {
-    let password = input.parse::<Password>()?;
+fn part1(input: &str) -> Result<String> {
     todo!()
-}
-
-struct PasswordIter {
-    from: Password,
-    to: Password,
-}
-
-impl PasswordIter {
-    /// Get the number of passwords that will be produced by this iterator
-    fn len(&self) -> usize {
-        self.from.distance(&self.to)
-    }
-}
-
-impl ParallelIterator for PasswordIter {
-    type Item = Password;
-
-    fn drive_unindexed<C>(self, consumer: C) -> C::Result
-    where
-        C: plumbing::UnindexedConsumer<Self::Item>,
-    {
-        self.drive(consumer)
-    }
-}
-
-impl IndexedParallelIterator for PasswordIter {
-    fn len(&self) -> usize {
-        self.len()
-    }
-
-    fn drive<C: plumbing::Consumer<Self::Item>>(self, consumer: C) -> C::Result {
-        let len = self.len();
-
-        if len <= 64 {
-            bridge(self, consumer)
-        } else {
-            let (left_iter, right_iter) = self.split_at(len / 2);
-            let (left_consumer, right_consumer, reducer) = consumer.split_at(len / 2);
-
-            let left = bridge(left_iter, left_consumer);
-            let right = bridge(right_iter, right_consumer);
-
-            reducer.reduce(left, right)
-        }
-    }
-
-    fn with_producer<CB: plumbing::ProducerCallback<Self::Item>>(self, callback: CB) -> CB::Output {
-        callback.callback(self)
-    }
-}
-
-impl Iterator for PasswordIter {
-    type Item = Password;
-
-    fn next(&mut self) -> Option<Self::Item> {
-        if self.from == self.to {
-            None
-        } else {
-            let out = self.from;
-            self.from.increment();
-            Some(out)
-        }
-    }
-}
-
-impl DoubleEndedIterator for PasswordIter {
-    fn next_back(&mut self) -> Option<Self::Item> {
-        if self.from == self.to {
-            None
-        } else {
-            let out = self.to;
-            self.to.decrement();
-            Some(out)
-        }
-    }
-}
-
-impl ExactSizeIterator for PasswordIter {
-    fn len(&self) -> usize {
-        self.from.distance(&self.to)
-    }
-}
-
-impl Producer for PasswordIter {
-    type Item = Password;
-
-    type IntoIter = PasswordIter;
-
-    fn into_iter(self) -> Self::IntoIter {
-        self
-    }
-
-    fn split_at(self, index: usize) -> (Self, Self) {
-        let halfway_base10 = self.from.as_base10() + index;
-    }
-}
-
-#[derive(Clone, Copy, PartialEq, Eq, PartialOrd, Ord)]
-struct Password([Letter; 8]);
-
-impl IntoParallelIterator for Password {
-    type Iter = PasswordIter;
-
-    type Item = Password;
-
-    fn into_par_iter(self) -> Self::Iter {
-        let from = self;
-        let mut to = self;
-        to.decrement();
-
-        PasswordIter { from, to }
-    }
-}
-
-impl Password {
-    const AAAAAAAA: Password = Password([Letter::A; 8]);
-    const ZZZZZZZZ: Password = Password([Letter::Z; 8]);
-
-    #[inline]
-    fn iter(&self) -> std::slice::Iter<'_, Letter> {
-        self.0.iter()
-    }
-
-    #[inline]
-    fn iter_mut(&mut self) -> std::slice::IterMut<'_, Letter> {
-        self.0.iter_mut()
-    }
-
-    fn increment(&mut self) {
-        for letter in self.iter_mut().rev() {
-            if let Some(next_letter) = letter.next() {
-                *letter = next_letter;
-                return;
-            } else {
-                // Overflow from `Z` to `A` and keep going
-                *letter = Letter::A;
-            }
-        }
-    }
-
-    fn decrement(&mut self) {
-        for letter in self.iter_mut().rev() {
-            if let Some(prev_letter) = letter.next() {
-                *letter = prev_letter;
-                return;
-            } else {
-                // Underflow from `A` to `Z` and keep going
-                *letter = Letter::Z;
-            }
-        }
-    }
-
-    fn as_base10(&self) -> usize {
-        self.iter()
-            .rev()
-            .enumerate()
-            .map(|(i, letter)| {
-                let n = letter.as_base10() as usize;
-                let exp = i as u32;
-                let base = 26usize;
-                n * base.pow(exp)
-            })
-            .sum()
-    }
-
-    fn from_base10(num: usize) -> Self {
-        let mut buf = itoa::Buffer::new();
-        let bytes = buf.format(num).as_bytes();
-
-        let inner = std::array::from_fn(|i| {
-            const ASCII_NUMBER_OFFSET: u8 = b'0';
-            let exp = (7 - i) as u32;
-
-            let digit = (bytes[i] - ASCII_NUMBER_OFFSET) as usize;
-            digit * 26usize.pow(exp)
-        });
-
-        Self(inner)
-        s.as_bytes().iter().rev().map(|ascii| ascii - ASCII_NUMBER_OFFSET).enumerate();
-    }
-
-    /// Returns the "distance" between two passwords, i.e. how many increments are required to go from
-    /// `self` to `to`.
-    fn distance(&self, to: &Password) -> usize {
-        const MAX: usize = const {
-            let mut max = 0;
-            let mut exp = 0;
-
-            loop {
-                if exp == 8 {
-                    break;
-                }
-
-                max += 25 * 26usize.pow(exp);
-                exp += 1;
-            }
-
-            max
-        };
-
-        let this = self.as_base10();
-        let other = to.as_base10();
-
-        if this < other {
-            other - this
-        } else {
-            MAX - this + other
-        }
-    }
-}
-
-impl Display for Password {
-    #[inline]
-    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
-        self.0.iter().try_for_each(|letter| Display::fmt(letter, f))
-    }
-}
-
-impl FromStr for Password {
-    type Err = Report;
-
-    fn from_str(s: &str) -> std::result::Result<Self, Self::Err> {
-        if !(s.len() == 8 && s.is_ascii()) {
-            bail!("password must be 8 ascii characters");
-        }
-
-        let mut letters = [Letter::A; 8];
-        s.char_indices().try_for_each(|(i, char)| {
-            char.try_into().map(|letter| {
-                letters[i] = letter;
-            })
-        })?;
-
-        Ok(Password(letters))
-    }
-}
-
-impl Index<usize> for Password {
-    type Output = Letter;
-
-    #[inline]
-    fn index(&self, index: usize) -> &Self::Output {
-        self.0.index(index)
-    }
-}
-
-impl IndexMut<usize> for Password {
-    #[inline]
-    fn index_mut(&mut self, index: usize) -> &mut Self::Output {
-        self.0.index_mut(index)
-    }
 }
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord)]
@@ -302,135 +46,209 @@ enum Letter {
     Z,
 }
 
-impl TryFrom<char> for Letter {
-    type Error = Report;
-
-    fn try_from(value: char) -> std::result::Result<Self, Self::Error> {
-        match value {
-            'a' => Ok(Letter::A),
-            'b' => Ok(Letter::B),
-            'c' => Ok(Letter::C),
-            'd' => Ok(Letter::D),
-            'e' => Ok(Letter::E),
-            'f' => Ok(Letter::F),
-            'g' => Ok(Letter::G),
-            'h' => Ok(Letter::H),
-            'i' => Ok(Letter::I),
-            'j' => Ok(Letter::J),
-            'k' => Ok(Letter::K),
-            'l' => Ok(Letter::L),
-            'm' => Ok(Letter::M),
-            'n' => Ok(Letter::N),
-            'o' => Ok(Letter::O),
-            'p' => Ok(Letter::P),
-            'q' => Ok(Letter::Q),
-            'r' => Ok(Letter::R),
-            's' => Ok(Letter::S),
-            't' => Ok(Letter::T),
-            'u' => Ok(Letter::U),
-            'v' => Ok(Letter::V),
-            'w' => Ok(Letter::W),
-            'x' => Ok(Letter::X),
-            'y' => Ok(Letter::Y),
-            'z' => Ok(Letter::Z),
-            other => bail!("invalid letter {other}, must be ascii lowercase"),
+impl Letter {
+    /// Create a new `Letter` from its `char` representation.
+    ///
+    /// # Errors
+    ///
+    /// Returns an error if the given `char` isn't an ASCII lowercase letter.
+    #[inline(always)]
+    const fn from_char(letter: char) -> Result<Self, &'static str> {
+        if letter.is_ascii_lowercase() {
+            // Safety: letter is ascii lowercase
+            Ok(unsafe { Letter::from_char_unchecked(letter) })
+        } else {
+            Err("letters must be ascii lowercase")
         }
     }
-}
 
-impl Letter {
+    /// Create a new `Letter` from its `char` representation.
+    ///
+    /// # Safety
+    ///
+    /// The caller must guarantee the given `char` is an ASCII lowercase letter.
+    #[inline(always)]
+    const unsafe fn from_char_unchecked(letter: char) -> Self {
+        let byte = letter as u8;
+        // Safety: the caller guarantees the letter is ascii lowercase
+        unsafe { std::mem::transmute::<u8, Letter>(byte) }
+    }
+
+    /// Create a new `Letter` from its `value` representation.
+    ///
+    /// # Errors
+    ///
+    /// Returns an error if the given value isn't between 0-25.
+    #[inline(always)]
+    const fn from_value(value: u8) -> Result<Self, &'static str> {
+        if value < 26 {
+            // Safety: the letter is between 0-25
+            Ok(unsafe { Letter::from_value_unchecked(value) })
+        } else {
+            Err("letter values are between 0-25")
+        }
+    }
+
+    /// Create a new `Letter` from its `value` representation
+    ///
+    /// # Safety
+    ///
+    /// The caller must guarantee the value is between 0-25.
+    #[inline(always)]
+    const unsafe fn from_value_unchecked(value: u8) -> Self {
+        let byte = value + ASCII_LETTER_OFFSET;
+        // Safety: the caller guarantees the value is between 0-25
+        unsafe { std::mem::transmute::<u8, Letter>(byte) }
+    }
+
     /// Returns the [`char`] representation of this `Letter`
-    #[inline]
-    fn as_char(&self) -> char {
+    #[inline(always)]
+    const fn char(&self) -> char {
         *self as u8 as char
     }
 
-    /// Returns the base 10 representation of this `Letter`
-    #[inline]
-    fn as_base10(&self) -> u8 {
-        const ASCII_LETTER_OFFSET: u8 = b'a';
+    /// Returns the base 10 value of this `Letter`, where `Letter::A.value() == 0` and `Letter::Z.value() == 25`.
+    #[inline(always)]
+    const fn value(&self) -> u8 {
         *self as u8 - ASCII_LETTER_OFFSET
     }
 
-    /// Returns the next `Letter` after this one.
+    /// Increments this `Letter` by 1 wrapping at `Z`.
     ///
-    /// Returns `None` if there's no next letter, i.e. when overflowing from `Z` to `A`.
-    #[inline]
-    fn next(&self) -> Option<Letter> {
-        match *self {
-            Letter::A => Some(Letter::B),
-            Letter::B => Some(Letter::C),
-            Letter::C => Some(Letter::D),
-            Letter::D => Some(Letter::E),
-            Letter::E => Some(Letter::F),
-            Letter::F => Some(Letter::G),
-            Letter::G => Some(Letter::H),
-            Letter::H => Some(Letter::I),
-            Letter::I => Some(Letter::J),
-            Letter::J => Some(Letter::K),
-            Letter::K => Some(Letter::L),
-            Letter::L => Some(Letter::M),
-            Letter::M => Some(Letter::N),
-            Letter::N => Some(Letter::O),
-            Letter::O => Some(Letter::P),
-            Letter::P => Some(Letter::Q),
-            Letter::Q => Some(Letter::R),
-            Letter::R => Some(Letter::S),
-            Letter::S => Some(Letter::T),
-            Letter::T => Some(Letter::U),
-            Letter::U => Some(Letter::V),
-            Letter::V => Some(Letter::W),
-            Letter::W => Some(Letter::X),
-            Letter::X => Some(Letter::Y),
-            Letter::Y => Some(Letter::Z),
-            Letter::Z => None,
-        }
+    /// Returns `true` if the operation wrapped.
+    #[inline(always)]
+    fn increment(&mut self) -> bool {
+        *self += 1u8;
+
+        *self == Letter::A
     }
 
-    #[inline]
-    fn wrapping_next(&self) -> Letter {
-        self.next().unwrap_or(Letter::A)
-    }
-
-    /// Returns the previous `Letter` before this one.
+    /// Decrements this `Letter` by 1, wrapping at `A`.
     ///
-    /// Returns `None` if there's no previous letter, i.e. when underflowing from `A` to `Z`.
-    fn previous(&self) -> Option<Letter> {
-        match *self {
-            Letter::A => None,
-            Letter::B => Some(Letter::A),
-            Letter::C => Some(Letter::B),
-            Letter::D => Some(Letter::C),
-            Letter::E => Some(Letter::D),
-            Letter::F => Some(Letter::E),
-            Letter::G => Some(Letter::F),
-            Letter::H => Some(Letter::G),
-            Letter::I => Some(Letter::H),
-            Letter::J => Some(Letter::I),
-            Letter::K => Some(Letter::J),
-            Letter::L => Some(Letter::K),
-            Letter::M => Some(Letter::L),
-            Letter::N => Some(Letter::M),
-            Letter::O => Some(Letter::N),
-            Letter::P => Some(Letter::O),
-            Letter::Q => Some(Letter::P),
-            Letter::R => Some(Letter::Q),
-            Letter::S => Some(Letter::R),
-            Letter::T => Some(Letter::S),
-            Letter::U => Some(Letter::T),
-            Letter::V => Some(Letter::U),
-            Letter::W => Some(Letter::V),
-            Letter::X => Some(Letter::W),
-            Letter::Y => Some(Letter::X),
-            Letter::Z => Some(Letter::Y),
-        }
+    /// Returns `true` if the operation wrapped.
+    #[inline(always)]
+    fn decrement(&mut self) -> bool {
+        *self -= 1u8;
+
+        *self == Letter::Z
     }
 }
+
+impl TryFrom<char> for Letter {
+    type Error = Report;
+
+    #[inline]
+    fn try_from(value: char) -> std::result::Result<Self, Self::Error> {
+        Letter::from_char(value).map_err(Report::msg)
+    }
+}
+
+impl PartialEq<char> for Letter {
+    #[inline(always)]
+    fn eq(&self, other: &char) -> bool {
+        self.char().eq(other)
+    }
+}
+
+impl FromStr for Letter {
+    type Err = Report;
+
+    fn from_str(s: &str) -> Result<Self> {
+        if s.len() > 1 {
+            bail!("Can't parse a letter from a multibyte string");
+        }
+
+        let ch = s
+            .chars()
+            .next()
+            .ok_or_else(|| eyre!("Can't parse a letter from an empty string"))?;
+        ch.try_into()
+    }
+}
+
+macro_rules! impl_letter_math {
+    ($($ty:ty),+) => {$(
+        impl Add<$ty> for Letter {
+            type Output = Letter;
+
+            fn add(self, rhs: $ty) -> Letter {
+                // By adding 26 to the left-hand side and taking the mod 26 of the right hand side,
+                // we wrap around 26 and ensure the value is always positive.
+
+                // Safety: `0-25` fits in every integer type
+                let lhs: $ty = unsafe { self.value().try_into().unwrap_unchecked() };
+                let rhs = rhs % 26;
+                let sum = (lhs + 26 + rhs) % 26;
+
+                // Safety: `sum` is guaranteed to be between 0-25 because it's mod 26.
+                unsafe {
+                    let value = sum.try_into().unwrap_unchecked();
+                    Letter::from_value_unchecked(value)
+                }
+            }
+        }
+
+        impl AddAssign<$ty> for Letter {
+            #[inline(always)]
+            fn add_assign(&mut self, rhs: $ty) {
+                *self = *self + rhs
+            }
+        }
+
+        impl Sub<$ty> for Letter {
+            type Output = Letter;
+
+            fn sub(self, rhs: $ty) -> Letter {
+                // By adding 26 to the left-hand side and taking the mod 26 of the right hand side,
+                // we wrap around 26 and ensure the value is always positive.
+
+                // Safety: `0-25` fits in every integer type
+                let lhs: $ty = unsafe { self.value().try_into().unwrap_unchecked() };
+                let rhs = rhs % 26;
+                let difference = (lhs + 26 - rhs) % 26;
+
+                // Safety: `sum` is guaranteed to be between 0-25 because it's mod 26.
+                unsafe {
+                    let value = difference.try_into().unwrap_unchecked();
+                    Letter::from_value_unchecked(value)
+                }
+            }
+        }
+
+        impl SubAssign<$ty> for Letter {
+            #[inline(always)]
+            fn sub_assign(&mut self, rhs: $ty) {
+                *self = *self - rhs;
+            }
+        }
+
+        impl TryFrom<$ty> for Letter {
+            type Error = ::eyre::Report;
+
+            #[inline(always)]
+            fn try_from(value: $ty) -> Result<Self, Self::Error> {
+                let value = value.try_into()?;
+                Self::from_value(value).map_err(Report::msg)
+            }
+        }
+
+        impl PartialEq<$ty> for Letter {
+            #[inline(always)]
+            fn eq(&self, other: &$ty) -> bool {
+                // Safety: 0-25 fits in every integer representation
+                let this: $ty = unsafe { self.value().try_into().unwrap_unchecked() };
+                this.eq(other)
+            }
+        }
+    )+};
+}
+
+impl_letter_math!(u8, u16, u32, u64, usize, u128, i8, i16, i32, i64, isize, i128);
 
 impl Display for Letter {
     #[inline]
     fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
-        f.write_char(self.as_char())
+        f.write_char(self.char())
     }
 }
 
@@ -438,18 +256,38 @@ impl Display for Letter {
 mod test {
     use super::*;
 
-    #[test]
-    fn letter_base10() {
-        assert_eq!(Letter::A.as_base10(), 0);
-        assert_eq!(Letter::Z.as_base10(), 25);
-    }
+    mod letter_math {
+        use super::*;
+        use pretty_assertions::assert_eq;
 
-    #[test]
-    fn password_base10() {
-        assert_eq!(Password::AAAAAAAA.as_base10(), 0);
-        assert_eq!(
-            Password::ZZZZZZZZ.as_base10(),
-            (0..8).map(|exp| 25 * 26usize.pow(exp)).sum()
-        )
+        #[test]
+        fn add() {
+            assert_eq!(Letter::A + 0, Letter::A);
+            assert_eq!(Letter::B + 5, Letter::G);
+        }
+
+        #[test]
+        fn add_wrapping() {
+            assert_eq!(Letter::Z + 1, Letter::A);
+            assert_eq!(Letter::A + 26, Letter::A);
+        }
+
+        #[test]
+        fn add_negative() {
+            assert_eq!(Letter::A + -1, Letter::Z);
+            assert_eq!(Letter::Z + -20, Letter::F);
+        }
+
+        #[test]
+        fn sub() {
+            assert_eq!(Letter::J - 1, Letter::I);
+            assert_eq!(Letter::M - 0, Letter::M);
+        }
+
+        #[test]
+        fn sub_wrapping() {
+            assert_eq!(Letter::C - 3, Letter::Z);
+            assert_eq!(Letter::G - 26, Letter::G);
+        }
     }
 }
