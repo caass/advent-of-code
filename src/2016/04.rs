03e96949bdfaa7e9f7d284ccb6ddee9d89cca0a2
Cass Fridkin <cass@swag.lgbt>
2024-10-25 00:06:27 -0600

    Solve day 4

--- src/2016/04.rs
+++ src/2016/04.rs
@@ -1,22 +1,114 @@
 use std::{
-    fmt::{self, Display, Formatter},
+    collections::HashMap,
+    fmt::{self, Display, Formatter, Write},
+    hash::Hash,
+    iter::repeat,
     mem::transmute,
+    ops::{AddAssign, Deref},
     str::{self, FromStr},
 };
 
 use eyre::{bail, eyre, OptionExt, Report, Result};
 use itertools::Itertools;
+use nohash_hasher::BuildNoHashHasher;
 use rayon::prelude::*;
 
 use crate::meta::Problem;
 
-pub const SECURITY_THROUGH_OBSCURITY: Problem = Problem::partially_solved(&|input| todo!());
+pub const SECURITY_THROUGH_OBSCURITY: Problem = Problem::solved(
+    &|input| {
+        input
+            .trim()
+            .par_lines()
+            .map(|line| {
+                line.parse().map(|room: Room| {
+                    room.is_real()
+                        .then_some(room.sector.into())
+                        .unwrap_or_default()
+                })
+            })
+            .try_reduce(|| 0usize, |a, b| Ok(a + b))
+    },
+    &|input| {
+        input
+            .trim()
+            .par_lines()
+            .map(|line| {
+                line.parse().map(|room: Room| {
+                    let sector = room.sector;
+                    if room.is_real() && room.decrypt() == ["northpole", "object", "storage"] {
+                        Some(sector)
+                    } else {
+                        None
+                    }
+                })
+            })
+            .reduce(
+                || Ok(None),
+                |a, b| match (a, b) {
+                    (Err(e), _) | (_, Err(e)) => Err(e),
+                    (Ok(Some(_)), Ok(Some(_))) => bail!("found two rooms with northpole objects"),
+                    (Ok(Some(sector)), _) | (_, Ok(Some(sector))) => Ok(Some(sector)),
+                    (Ok(None), Ok(None)) => Ok(None),
+                },
+            )?
+            .ok_or_eyre("no rooms with northpole objects")
+    },
+);
 
 #[derive(Debug)]
 struct Room {
     name: EncryptedName,
     sector: u16,
     checksum: Checksum,
+}
+
+impl Room {
+    fn is_real(&self) -> bool {
+        let mut frequencies: HashMap<Letter, u8, BuildNoHashHasher<Letter>> = HashMap::default();
+
+        self.name
+            .letters()
+            .for_each(|letter| frequencies.entry(letter).or_default().add_assign(1));
+
+        let Some((a, b, c, d, e)) = frequencies
+            .into_iter()
+            .map(|(letter, count)| LetterFrequency { letter, count })
+            .sorted()
+            .rev()
+            .map(|LetterFrequency { letter, .. }| letter)
+            .take(5)
+            .collect_tuple()
+        else {
+            return false;
+        };
+
+        Checksum([a, b, c, d, e]) == self.checksum
+    }
+
+    fn decrypt(self) -> DecryptedName {
+        self.name.decrypt(self.sector)
+    }
+}
+
+#[derive(Debug, PartialEq, Eq)]
+struct LetterFrequency {
+    letter: Letter,
+    count: u8,
+}
+
+impl PartialOrd for LetterFrequency {
+    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+impl Ord for LetterFrequency {
+    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
+        self.count
+            .cmp(&other.count)
+            .then_with(|| other.letter.cmp(&self.letter))
+    }
 }
 
 impl FromStr for Room {
@@ -51,6 +143,17 @@ impl Display for Room {
 #[derive(Debug)]
 struct EncryptedName(Vec<NamePart>);
 
+impl EncryptedName {
+    fn letters(&self) -> impl Iterator<Item = Letter> + use<'_> {
+        self.0.iter().flat_map(|part| part.letters())
+    }
+
+    fn decrypt(mut self, shift: u16) -> DecryptedName {
+        self.0.iter_mut().for_each(|part| part.decrypt(shift));
+        DecryptedName(self.0)
+    }
+}
+
 impl FromIterator<NamePart> for EncryptedName {
     fn from_iter<T: IntoIterator<Item = NamePart>>(iter: T) -> Self {
         Self(Vec::from_iter(iter))
@@ -67,13 +170,50 @@ impl FromStr for EncryptedName {
 
 impl Display for EncryptedName {
     fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
-        Itertools::intersperse(self.0.iter().map(NamePart::as_ref), "-")
+        Itertools::intersperse(self.0.iter().map(NamePart::deref), "-")
             .try_for_each(|data| f.write_str(data))
+    }
+}
+
+struct DecryptedName(Vec<NamePart>);
+
+impl<'a, T: AsRef<[&'a str]>> PartialEq<T> for DecryptedName {
+    fn eq(&self, other: &T) -> bool {
+        let other = other.as_ref();
+        self.0.len() == other.len()
+            && self
+                .0
+                .iter()
+                .map(NamePart::deref)
+                .zip(other)
+                .all(|(a, &b)| a == b)
+    }
+}
+
+impl Display for DecryptedName {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        self.0
+            .iter()
+            .map(NamePart::deref)
+            .interleave_shortest(repeat(" "))
+            .try_for_each(|s| f.write_str(s))
     }
 }
 
 #[derive(Debug)]
 struct NamePart(Vec<Letter>);
+
+impl NamePart {
+    fn letters(&self) -> impl Iterator<Item = Letter> + use<'_> {
+        self.0.iter().copied()
+    }
+
+    fn decrypt(&mut self, shift: u16) {
+        self.0
+            .iter_mut()
+            .for_each(|letter| *letter = letter.rotate(shift));
+    }
+}
 
 impl FromIterator<Letter> for NamePart {
     fn from_iter<T: IntoIterator<Item = Letter>>(iter: T) -> Self {
@@ -89,8 +229,10 @@ impl FromStr for NamePart {
     }
 }
 
-impl AsRef<str> for NamePart {
-    fn as_ref(&self) -> &str {
+impl Deref for NamePart {
+    type Target = str;
+
+    fn deref(&self) -> &Self::Target {
         let bytes = unsafe { transmute::<&[Letter], &[u8]>(&self.0) };
         unsafe { str::from_utf8_unchecked(bytes) }
     }
@@ -98,11 +240,12 @@ impl AsRef<str> for NamePart {
 
 impl Display for NamePart {
     fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
-        f.write_str(self.as_ref())
+        f.write_str(self)
     }
 }
 
-#[derive(Debug, Clone, Copy)]
+#[allow(dead_code, reason = "not actually dead")]
+#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord)]
 #[repr(u8)]
 enum Letter {
     A = b'a',
@@ -133,6 +276,36 @@ enum Letter {
     Z,
 }
 
+impl Letter {
+    /// Rotate a letter by a given amount
+    fn rotate(self, by: u16) -> Self {
+        let ascii_byte = self as u8;
+        let zeroed_byte = ascii_byte - b'a';
+
+        let shifted = zeroed_byte as u16 + by;
+        let rotated_byte = (shifted % 26) as u8;
+
+        let shifted_ascii_byte = rotated_byte + b'a';
+
+        debug_assert!(shifted_ascii_byte.is_ascii());
+        unsafe { std::mem::transmute::<u8, Letter>(shifted_ascii_byte) }
+    }
+}
+
+impl Hash for Letter {
+    fn hash<H: std::hash::Hasher>(&self, state: &mut H) {
+        state.write_u8(*self as u8);
+    }
+}
+
+impl nohash_hasher::IsEnabled for Letter {}
+
+impl Display for Letter {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        f.write_char(*self as u8 as char)
+    }
+}
+
 impl TryFrom<char> for Letter {
     type Error = Report;
 
@@ -148,7 +321,7 @@ impl TryFrom<char> for Letter {
     }
 }
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq, Eq)]
 struct Checksum([Letter; 5]);
 
 impl FromStr for Checksum {
@@ -168,15 +341,17 @@ impl FromStr for Checksum {
     }
 }
 
-impl AsRef<str> for Checksum {
-    fn as_ref(&self) -> &str {
-        let bytes = unsafe { transmute::<&[Letter; 5], &[u8; 5]>(&self.0) };
+impl Deref for Checksum {
+    type Target = str;
+
+    fn deref(&self) -> &Self::Target {
+        let bytes = unsafe { transmute::<&[Letter], &[u8]>(&self.0) };
         unsafe { str::from_utf8_unchecked(bytes) }
     }
 }
 
 impl Display for Checksum {
     fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
-        f.write_str(self.as_ref())
+        f.write_str(self)
     }
 }
