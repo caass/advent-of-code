68fcf7cca338725adec3ed81452b8ec71ef6be4d
Cass Fridkin <cass@swag.lgbt>
2025-03-16 12:16:01 -0600

    Add stubbed out years to `aoc`

--- aoc/src/lib.rs
+++ aoc/src/lib.rs
@@ -3,4 +3,11 @@ use aoc_meta::{AdventOfCode, Year};
 pub const AOC: AdventOfCode = AdventOfCode::new()
     .with_year(Year::Fifteen, aoc_2015::PROBLEMS)
     .with_year(Year::Sixteen, aoc_2016::PROBLEMS)
+    .with_year(Year::Seventeen, aoc_2016::PROBLEMS)
+    .with_year(Year::Eighteen, aoc_2016::PROBLEMS)
+    .with_year(Year::Nineteen, aoc_2016::PROBLEMS)
+    .with_year(Year::Twenty, aoc_2024::PROBLEMS)
+    .with_year(Year::TwentyOne, aoc_2024::PROBLEMS)
+    .with_year(Year::TwentyTwo, aoc_2024::PROBLEMS)
+    .with_year(Year::TwentyThree, aoc_2024::PROBLEMS)
     .with_year(Year::TwentyFour, aoc_2024::PROBLEMS);
