f84dcf2849df29f88b469d58d8257b5f3ad1adcb
Cass Fridkin <cass@swag.lgbt>
2025-11-16 07:37:07 -0700

    minor cleanups

--- aoc-2016/src/20.rs
+++ aoc-2016/src/20.rs
@@ -1,4 +1,4 @@
-use std::{ops::RangeInclusive, str::FromStr};
+use std::str::FromStr;
 
 use eyre::{OptionExt, Report, Result};
 use itertools::Itertools;
@@ -7,43 +7,61 @@ use rayon::prelude::*;
 use aoc_meta::Problem;
 
 pub const FIREWALL_RULES: Problem = Problem::solved(
-    &|input| input.parse().and_then(Firewall::lowest_allowed_ip),
+    &|input| {
+        let fw = input.parse::<Firewall>()?;
+        fw.lowest_allowed_ip().ok_or_eyre("no IPs allowed!")
+    },
     &|input| input.parse().map(Firewall::count_allowed_ips),
 );
 
+#[derive(Debug, Clone, Copy)]
+struct IpRange {
+    start: u32,
+    end: u32,
+}
+
+impl FromStr for IpRange {
+    type Err = Report;
+
+    fn from_str(s: &str) -> Result<Self> {
+        let parts = s.split_once('-').ok_or_eyre("no '-' in input")?;
+        let start = parts.0.parse()?;
+        let end = parts.1.parse()?;
+
+        Ok(IpRange { start, end })
+    }
+}
+
 #[derive(Debug, Clone)]
 struct Firewall {
-    inner: Vec<RangeInclusive<u32>>,
+    inner: Vec<IpRange>,
 }
 
 impl Firewall {
-    fn first(&self) -> Option<RangeInclusive<u32>> {
-        self.inner.first().cloned()
+    fn first(&self) -> Option<IpRange> {
+        self.inner.first().copied()
     }
 
-    fn lowest_allowed_ip(self) -> Result<u32> {
-        if self.first().is_none_or(|range| *range.start() > 0) {
-            return Ok(0);
+    fn lowest_allowed_ip(self) -> Option<u32> {
+        if self.first().is_none_or(|range| range.start > 0) {
+            return Some(0);
         }
 
         self.into_iter()
             .next()
-            .ok_or_eyre("empty firewall?")?
-            .end()
+            .expect("at least one firewall after first() check")
+            .end
             .checked_add(1)
-            .ok_or_eyre("no allowed ips")
     }
 
     fn count_allowed_ips(self) -> u32 {
-        let Some(allowed_count) = self.first().map(|r| *r.start()) else {
+        let Some(k) = self.first().map(|r| r.start) else {
             return u32::MAX;
         };
 
         self.into_iter()
             .tuple_windows()
-            .fold(allowed_count, |n, (a, b)| {
-                n + b.start() - a.end().saturating_add(1)
-            })
+            .fold(k, |n, (a, b)| n + b.start - a.end.saturating_add(1))
     }
 }
 
@@ -53,16 +71,10 @@ impl FromStr for Firewall {
     fn from_str(s: &str) -> Result<Self> {
         let mut ranges = s
             .par_lines()
-            .map(|line| {
-                let parts = line.split_once('-').ok_or_eyre("no '-' in input")?;
-                let start = parts.0.parse()?;
-                let end = parts.1.parse()?;
+            .map(IpRange::from_str)
+            .collect::<Result<Vec<_>>>()?;
 
-                Ok(start..=end)
-            })
-            .collect::<Result<Vec<RangeInclusive<u32>>>>()?;
-
-        ranges.par_sort_unstable_by_key(|range| *range.start());
+        ranges.par_sort_unstable_by_key(|range| range.start);
 
         Ok(Self { inner: ranges })
     }
@@ -70,7 +82,7 @@ impl FromStr for Firewall {
 
 impl IntoIterator for Firewall {
     type IntoIter = MergedFirewall;
-    type Item = RangeInclusive<u32>;
+    type Item = IpRange;
 
     fn into_iter(self) -> Self::IntoIter {
         MergedFirewall {
@@ -81,26 +93,26 @@ impl IntoIterator for Firewall {
 }
 
 struct MergedFirewall {
-    range: Option<RangeInclusive<u32>>,
-    inner: std::vec::IntoIter<RangeInclusive<u32>>,
+    range: Option<IpRange>,
+    inner: std::vec::IntoIter<IpRange>,
 }
 
 impl Iterator for MergedFirewall {
-    type Item = RangeInclusive<u32>;
+    type Item = IpRange;
 
     fn next(&mut self) -> Option<Self::Item> {
-        let mut old = self.range.clone().or_else(|| self.inner.next())?;
+        let mut old = self.range.or_else(|| self.inner.next())?;
         let mut new = None;
 
         for range in self.inner.by_ref() {
-            if old.end().saturating_add(1) < *range.start() {
+            if old.end.saturating_add(1) < range.start {
                 new = Some(range);
                 break;
             }
 
-            let start = u32::min(*old.start(), *range.start());
-            let end = u32::max(*old.end(), *range.end());
-            old = start..=end;
+            let start = u32::min(old.start, range.start);
+            let end = u32::max(old.end, range.end);
+            old = IpRange { start, end }
         }
 
         self.range = new;
