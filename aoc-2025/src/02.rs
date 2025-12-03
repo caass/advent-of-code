465aece2a212a7e255efec511f94e8569a9ae66d
Cass Fridkin <cass@swag.lgbt>
2025-12-02 21:35:09 -0700

    part 2 of day 2 of 2025

--- aoc-2025/src/02.rs
+++ aoc-2025/src/02.rs
@@ -1,9 +1,18 @@
 use eyre::{OptionExt, bail};
+use itertools::Itertools;
 use rayon::prelude::*;
 
 use aoc_meta::Problem;
 
-pub const GIFT_SHOP: Problem = Problem::partially_solved(&|input| {
+pub const GIFT_SHOP: Problem = Problem::solved(
+    &|input| count_invalid_ids(input, doubled_numbers),
+    &|input| count_invalid_ids(input, repeated_numbers),
+);
+
+fn count_invalid_ids<F>(input: &str, f: F) -> Result<u64, eyre::Error>
+where
+    F: Fn(u64) -> bool + Send + Sync,
+{
     input
         .par_split(',')
         .map(|s| {
@@ -21,17 +30,8 @@ pub const GIFT_SHOP: Problem = Problem::partially_solved(&|input| {
             |total_sum, res| {
                 let range = res?;
 
-                let this_sum = range
-                    .into_par_iter()
-                    .filter(|&n| {
-                        let mut buf = itoa::Buffer::new();
-
-                        let s = buf.format(n);
-                        let midpoint = s.len() / 2;
-
-                        s[..midpoint] == s[midpoint..]
-                    })
-                    .sum::<u64>();
+                // TODO: this is slow, we shouldn't have to check every item. But i'm not gonna sweat it for now
+                let this_sum = range.into_par_iter().filter(|&n| f(n)).sum::<u64>();
 
                 total_sum
                     .checked_add(this_sum)
@@ -42,4 +42,25 @@ pub const GIFT_SHOP: Problem = Problem::partially_solved(&|input| {
             || 0,
             |a, b| a.checked_add(b).ok_or_eyre("attempt to add with overflow"),
         )
-});
+}
+
+fn doubled_numbers(n: u64) -> bool {
+    let mut buf = itoa::Buffer::new();
+
+    let s = buf.format(n);
+    let midpoint = s.len() / 2;
+
+    s[..midpoint] == s[midpoint..]
+}
+
+fn repeated_numbers(n: u64) -> bool {
+    let mut buf = itoa::Buffer::new();
+
+    let s = buf.format(n);
+    let midpoint = s.len() / 2;
+
+    (1..=midpoint).any(|i| {
+        let mut chunks = s.as_bytes().chunks_exact(i);
+        chunks.remainder().is_empty() && chunks.all_equal()
+    })
+}
