2adbbcf39056071513c6409bcfe6b1fc2210bb88
Cass Fridkin <cass@swag.lgbt>
2026-04-22 22:53:20 -0600

    solve 2016-22-2

--- aoc-2016/src/22.rs
+++ aoc-2016/src/22.rs
@@ -1,35 +1,91 @@
 use std::str::FromStr;
 
 use aoc_meta::Problem;
-use eyre::{OptionExt, Result, bail};
+use eyre::{OptionExt, Report, Result, bail};
 use itertools::Itertools;
 use rayon::prelude::*;
 
-pub const GRID_COMPUTING: Problem = Problem::partially_solved(&viable_pairs);
+pub const GRID_COMPUTING: Problem = Problem::solved(
+    &|input| input.parse().map(StorageCluster::viable_pairs),
+    &|input| input.parse().map(StorageCluster::minimum_steps),
+);
 
-fn viable_pairs(input: &str) -> Result<usize> {
-    let (_, after) = input
-        .split_once("Use%\n")
-        .ok_or_eyre("couldn't find \"Use%\\n\" in input")?;
+impl StorageCluster {
+    fn viable_pairs(self) -> usize {
+        self.nodes
+            .par_iter()
+            .copied()
+            .filter(|a| a.used > 0)
+            .flat_map(|a| {
+                self.nodes
+                    .par_iter()
+                    .copied()
+                    .filter(move |&b| !(a.x == b.x && a.y == b.y) && a.used <= b.available)
+            })
+            .count()
+    }
 
-    let nodes = after
-        .par_lines()
-        .map(PositionedNode::from_str)
-        .collect::<Result<Vec<_>>>()?;
+    fn minimum_steps(self) -> u16 {
+        let empty = self.nodes[self.empty_idx];
+        let target = self.nodes[self.target_idx];
 
-    let n = nodes
-        .par_iter()
-        .copied()
-        .filter(|a| a.used > 0)
-        .flat_map(|a| {
-            nodes
-                .par_iter()
-                .copied()
-                .filter(move |&b| !(a.x == b.x && a.y == b.y) && a.used <= b.available)
+        u16::from(empty.x + empty.y + target.x - 1) + u16::from(5 * target.x - 4)
+    }
+}
+
+#[derive(Debug)]
+struct StorageCluster {
+    target_idx: usize,
+    empty_idx: usize,
+    nodes: Vec<PositionedNode>,
+}
+
+impl FromStr for StorageCluster {
+    type Err = Report;
+
+    fn from_str(s: &str) -> Result<Self> {
+        let (_, after) = s
+            .split_once("Use%\n")
+            .ok_or_eyre("couldn't find \"Use%\\n\" in input")?;
+
+        let mut max_x = 0;
+        let mut empty_idx = 0;
+        let mut found_empty = false;
+
+        let nodes = after
+            .lines()
+            .map(PositionedNode::from_str)
+            .enumerate()
+            .inspect(|(i, res)| {
+                let Ok(node) = res else {
+                    return;
+                };
+
+                if node.used == 0 {
+                    found_empty = true;
+                    empty_idx = *i;
+                }
+
+                max_x = max_x.max(node.x)
+            })
+            .map(|(_, res)| res)
+            .collect::<Result<Vec<PositionedNode>>>()?;
+
+        if !found_empty {
+            bail!("cannot move target data")
+        }
+
+        let target_idx = nodes
+            .iter()
+            .position(|node| node.x == max_x && node.y == 0)
+            .ok_or_eyre("no target data")?;
+
+        Ok(Self {
+            target_idx,
+            empty_idx,
+            nodes,
         })
-        .count();
-
-    Ok(n)
+    }
 }
 
 #[derive(Debug, Clone, Copy)]
